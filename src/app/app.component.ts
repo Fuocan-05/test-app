bade64e6d1e4c0256411ba5fa35f9148321aeb06
GiovanniDMaio <giovannidimaiopalermo@gmail.com>
2025-09-11 23:15:48 +0200

    some changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,13 +2,14 @@ import { AfterViewInit, Component, ElementRef } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { BodyComponent } from './components/body/body.component';
-import { FormComponent } from "./form/form.component";
+import { FormComponent } from "./components/form/form.component";
 import { FooterComponent } from './components/footer/footer.component';
+import { ProgettiComponent } from "./components/progetti/progetti.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, NavbarComponent, BodyComponent, FormComponent, FooterComponent],
+  imports: [NavbarComponent, BodyComponent, FormComponent, FooterComponent, ProgettiComponent],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
